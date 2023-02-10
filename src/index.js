57177f88d69e23cb1e84190b43d385cb38f3e767
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2023-02-09 23:01:32 -0300

    Articles

--- src/index.js
+++ src/index.js
@@ -4,14 +4,35 @@ let path = require("path");
 let app = express();
 
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views")
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Index page
-app.get("", (req, res) => {
+// Routes start
+// Index
+app.get("/", (req, res) => {
     res.render(__dirname + "/views/index", { pageName: "Index" });
 });
+
+// Articles
+app.use('/articles', require(__dirname + '/routes/articles.routes.js'));
+
+// Contact
+app.get("/contact", (req, res) => {
+    res.render(__dirname + "/views/contact", { pageName: "Contact" });
+});
+
+// Contact
+app.get("/about", (req, res) => {
+    res.render(__dirname + "/views/about", { pageName: "About" });
+});
+
+// 404 Not Found
+app.get('*', function(req, res){
+    res.render(__dirname + "/views/404", { pageName: "404" });
+  });
+// Routes end
 
 app.listen(80, () => {
     console.log("Listening on port 80");
--- src/routes/articles.routes.js
+++ src/routes/articles.routes.js
@@ -1,2 +1,20 @@
 let express = require("express");
 let router = express.Router();
+
+router.get("/", (req, res) =>{
+    res.render("articles/articles.ejs", { pageName: "Articles" });
+});
+
+router.get("/infortunio-laboral", (req, res) => {
+    res.render("articles/infortunioLaboral.ejs", { pageName: "Articles" });
+});
+
+router.get("/asistencia-letrada", (req, res) => {
+    res.render("articles/asistenciaLetrada.ejs", { pageName: "Articles" });
+});
+
+router.get("/bonificacion-fuerzas", (req, res) => {
+    res.render("articles/bonificacionFuerzas.ejs", { pageName: "Articles" });
+});
+
+module.exports = router
